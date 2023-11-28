f79776c57f8eb96d392dcc01cf423d9dc5d48fe2
denis <kagane@yandex.ru>
2023-11-28 15:21:42 +0300

    добавил версию с Set от интервьювера

--- src/main/java/IntegerToInt/IntContainsInt2.java
+++ src/main/java/IntegerToInt/IntContainsInt2.java
@@ -10,20 +10,19 @@ class IntContainsInt2 {
         Set<Integer> set1 = Arrays.stream(nums1)
                 .boxed()
                 .collect(Collectors.toSet());
+        System.out.println(set1);
         Set<Integer> set2 = Arrays.stream(nums2)
                 .boxed()
                 .collect(Collectors.toSet());
-        set1.retainAll(set2);
-
-        System.out.println(set1);
         System.out.println(set2);
 
+        set1.retainAll(set2);
         return set1.stream()
                 .mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {4, 9, 5};
+        int[] nums1 = {4, 9, 5, 9};
         int[] nums2 = {9, 4, 9, 8, 4};
         int[] intersection = intersection(nums1, nums2);
         System.out.println(Arrays.stream(intersection).boxed().collect(Collectors.toList()));
